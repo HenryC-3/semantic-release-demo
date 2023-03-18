14e53219093ab46ed1b26521007df2d4716b33ed
HenryC-3 <2324375439@qq.com>
2023-03-18 19:01:34 +0800

    refactor: rename

---
+++ .releaserc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    branches: [
+        master,
+        {
+            name: "beta",
+            prerelease: true,
+        },
+    ],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "angular",
+                releaseRules: [
+                    { type: "docs", scope: "README", release: "patch" },
+                    { type: "refactor", release: "patch" },
+                    { type: "style", release: "patch" },
+                ],
+                parserOpts: {
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+                },
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github",
+    ],
+};
